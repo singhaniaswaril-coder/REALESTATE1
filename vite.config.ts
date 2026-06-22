b8e52dad90b631df4ed17b68b61d2a028515e36d
singhaniaswaril-coder <singhaniaswaril@gmail.com>
2026-06-23 01:05:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 //     error logger plugins, and sandbox detection (port/host/strictPort).
 // You can pass additional config via defineConfig({ vite: { ... }, etc... }) if needed.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-import { defineConfig } from 'vite'
+//import { defineConfig } from 'vite'
 
 export default defineConfig({
   tanstackStart: {
